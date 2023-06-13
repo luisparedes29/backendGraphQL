52acc085a4a05de74d1dc515bc3cd61cd1c3d090
saritagud <gudinop864@gmail.com>
2023-06-13 17:07:28 -0400

    mutaciones de eliminar, editar tweet y editar favoritos listas

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -10,6 +10,18 @@ export const resolvers ={
             const newTweet = new tweets({user,tweet,favorite})
             await newTweet.save()
             return newTweet
-        }
+        },
+        updateTweet: async (_, {id, tweet}) => {
+            const updateTweet = await tweets.findByIdAndUpdate(id, { tweet }, { new: true });
+            return updateTweet
+        },
+        deleteTweet: async (_, {id}) => {
+            const deleteTweet = await tweets.findByIdAndDelete(id);
+            return deleteTweet
+        },
+        updateFavorites: async(_, {id, favorite}) => {
+            const updateFavorites = await tweets.findByIdAndUpdate(id, { favorite }, { new: true })
+            return updateFavorites
+        } 
     }
 }
